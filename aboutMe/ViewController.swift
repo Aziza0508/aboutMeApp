6a32a4f6341abfa62dce94860f39fffa1cf40336
Aziza <aziza.gilash.atyrau@gmail.com>
2025-02-10 09:03:06 +0500

    Initial Commit

---
+++ aboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aboutMe
+//
+//  Created by Aziza Gilash on 10.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
